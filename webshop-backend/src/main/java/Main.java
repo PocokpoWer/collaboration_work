6eb9398b97033bf7162c81f9902db184e1eef3bc
Tibi <farkas.tibor1105@gmail.com>
2025-11-16 13:22:24 +0100

    refactored code

--- webshop-backend/src/main/java/Main.java
+++ webshop-backend/src/main/java/Main.java
@@ -1,22 +1,4 @@
-import com.webshop.model.Product;
-import com.webshop.model.ShoppingCart;
-import com.webshop.model.User;
-
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Ákos");
-        Product carrot = new Product("Carrot", 300, 5);
-        Product apple = new Product("Apple", 250, 0);
-
-        ShoppingCart cart = new ShoppingCart(user);
-
-        cart.addProduct(carrot);
-        cart.addProduct(carrot);
-        cart.addProduct(apple);
-
-        System.out.println(cart);
-
-        cart.removeProduct(carrot);
-        System.out.println(cart);
     }
 }
