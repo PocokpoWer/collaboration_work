cc06dbe2b07a1790b3a9e1e49075ad2e7e302a2f
Kriszti <csuka.tina@gmail.com>
2025-12-07 12:08:54 +0100

    Add MonetaryAmountValidator

---
+++ webshop-backend/src/main/java/MonetaryAmountValidator.java
@@ -0,0 +1,18 @@
+public class MonetaryAmountValidator implements Validator<MonetaryAmount> {
+
+    @Override
+    public boolean isValid(MonetaryAmount monetaryAmount) {
+        if (monetaryAmount == null) return false;
+
+        if (monetaryAmount.getAmount() <= 0) return false;
+
+        String currency = monetaryAmount.getCurrency();
+        if (currency == null) return false;
+
+        if (!(currency.equals("HUF") || currency.equals("EUR") || currency.equals("USD"))) {
+            return false;
+        }
+
+        return true;
+    }
+}
